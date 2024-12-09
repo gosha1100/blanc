063c354c274b7d176dbf970bb6e13aab46fc43d8
Julian Strohmeier <julian.strohmeier@protonmail.com>
2024-12-09 11:47:37 +0100

    intial

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // Your Next.js config here
 }
 
 export default withPayload(nextConfig)
